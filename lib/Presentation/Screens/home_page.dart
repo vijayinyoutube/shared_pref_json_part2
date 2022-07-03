d3feba81e6099e4c99a9920b7a12e3705f526d64
vijayinyoutube <vijaythevirtue@gmail.com>
2022-07-03 14:21:35 +0530

    refactor

--- lib/Presentation/Screens/home_page.dart
+++ lib/Presentation/Screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_pref_json_part2/Presentation/Components/spacer.dart';
 
 import '../../../../Data/Raw/raw_data.dart';
 import '../../Data/SharedPreferences/home_page_shared_pref.dart';
@@ -13,6 +14,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController nameController;
+  late TextEditingController ageController;
+  @override
+  void initState() {
+    nameController = TextEditingController();
+    ageController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    ageController.dispose();
+    super.dispose();
+  }
+
   HomePageData homePageData = HomePageData();
   @override
   Widget build(BuildContext context) {
@@ -21,14 +38,54 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await homePageData.saveJsonData(rawData);
-            homePageData.getJsonData();
-          },
-          child: const Text('Save Data'),
+        child: Padding(
+          padding: const EdgeInsets.all(20.00),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Storing data in shared pref as JSON'),
+              const HeightSpacer(myHeight: 25.00),
+              buildTextField(nameController, 'Your name'),
+              const HeightSpacer(myHeight: 25.00),
+              buildTextField(ageController, 'Your age'),
+              const HeightSpacer(myHeight: 25.00),
+              buildBtn()
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget buildTextField(TextEditingController controller, String labelText) =>
+      TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.circular(5.5),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.blue,
+            ),
+          ),
+          prefixIcon: const Icon(
+            Icons.person,
+            color: Colors.blue,
+          ),
+          filled: true,
+          fillColor: Colors.blue[50],
+          labelText: labelText,
+          labelStyle: const TextStyle(color: Colors.blue),
+        ),
+      );
+
+  Widget buildBtn() => ElevatedButton(
+        onPressed: () async {
+          await homePageData.saveJsonData(rawData);
+          homePageData.getJsonData();
+        },
+        child: const Text('Save Data'),
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Json and Shared Pref',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
